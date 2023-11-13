ff2162c697a7acd93a986441f27ca668ecfb0c23
Sama <101380876+HasnyS@users.noreply.github.com>
2023-11-13 18:00:02 -0500

    added about me section
    {BUG}
    padding colors not showing

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Hero } from "@/components/hero";
 import { Navbar } from "@/components/navbar";
+import AboutSection from "@/components/AboutSection";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <div className="container mt-24 mx-auto px-12 py-4">
                 <Navbar/>
                 <Hero />
+                <AboutSection/>
                 {/* Use the Image component here if needed */}
                 {/* <Image src="/your-image.jpg" width={500} height={300} alt="Your Image" /> */}
             </div>
